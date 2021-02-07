e37ef9d49705be8e885f6d14d8a49d79ba7625dd
martineleirvik <leirvikmartine@gmail.com>
2021-02-07 13:59:24 +0100

    q2, style

--- js/question-2.js
+++ js/question-2.js
@@ -1,4 +1,5 @@
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating"
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
 const resultsContainer = document.querySelector(".results");
 
@@ -6,23 +7,28 @@ async function getGames() {
 
     const response = await fetch(url);
 
-    const results = await response.json();
+    const content = await response.json();
 
-    const facts = results.all;
+    const games = content.results;
 
     resultsContainer.innerHTML = "";
+    
 
-    for(let i = 0; i < facts.length; i++) {
-        console.log(facts[i].text);
+    for(let i = 0; i < games.length; i++) {
+        console.log(games[i].name);
+        console.log(games[i].rating);
+        console.log(games[i].tags.length);
 
         if (i === 8) {
             break;
         }
 
-        resultsContainer.innerHTML += `<div class="results">${fatcs[i].text}<div>`;
+        resultsContainer.innerHTML += `<div class="result">${games[i].name}<div>`;
+        resultsContainer.innerHTML += `<div class="result">${games[i].rating}<div>`;
+        resultsContainer.innerHTML += `<div class="result">${games[i].tags.length}<div>`;
 
     }
 
 }
 
-getGames();
+    getGames();
